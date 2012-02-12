59e0663ff9803157874c4fb5731551cac57c3f4a
thebluber <thebluber@googlemail.com>
2012-02-12 16:51:27 +0100

    recursion failed...why

--- capy.js
+++ capy.js
@@ -1,41 +1,33 @@
 key = prompt("keyword");
 
 var doJsonP = function(url, callback) {
-     var request_url = url + "&callback=" + callback;
+     var call = callback;
+     var request_url = url + "&callback=" + call;
      var request = document.createElement("script");
      request.src = request_url;
      document.body.appendChild(request);
    };
 
-var setStart = function(){
-    var rand = Math.floor(Math.random() * 61);
-    return "&start=" + rand;
-   };
-
-
-var callback = function(json) {
-    var results = json.responseData.results;
-    var urls = [];
-    for(i = 0; i < results.length; i++) {
-      urls[i] = results[i].url;
-    };
-    getMore(urls);
-}
-
-
-var getMore = function(urls) {
+var getUrls = function(key, acc, max) {
     var rand = Math.floor(Math.random() * 61);
     var url = "https://ajax.googleapis.com/ajax/services/search/images?v=1.0&q=" + key + "&start=" + rand;
-    imgs = document.getElementsByTagName("img");
-    imgs.length == urls.length ? replace(urls) : doJsonP(url, "callback");    
+    var a = [];
+    doJsonP(url, function(json){var res = json.responseData.results;for(var i in res) {a.unshift(res[i].url);};});
+    if (max <= 0) {
+        replace(acc);
+    } else {
+        getUrls(key, acc.concat(a), max - 1);
+    }
 }
+
 
 var replace = function(urls) {
     var imgs = document.getElementsByTagName("img");
     for(i = 0; i < urls.length; i++){
-        imgs[i].src = urls[i];
+        var rand = Math.floor(Math.random() * urls.length);
+        imgs[i].src = urls[rand];
     }
 }
 
-url = "https://ajax.googleapis.com/ajax/services/search/images?v=1.0&q=" + key;
-window.onload = doJsonP(url, "callback");
+var max = document.getElementsByTagName("img").length
+window.onload = getUrls(key, [], max);
